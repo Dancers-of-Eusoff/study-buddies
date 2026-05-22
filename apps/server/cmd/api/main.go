f00deae15ecc7e9ec12b23433dfcb699e3aa4b28
KelvenN11 <kelvennathanael11@gmail.com>
2026-05-22 22:39:26 +0800

    Add backend for study room with golang memory

---
+++ apps/server/cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Dancers-of-Eusoff/study-buddies/apps/server/internal/rooms"
+)
+
+func main() {
+	roomRepo := rooms.NewMemoryRepository()
+	roomService := rooms.NewService(roomRepo)
+	roomHandler := rooms.NewHandler(roomService)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("Study Buddies backend is running"))
+	})
+
+	roomHandler.RegisterRoutes(mux)
+
+	log.Println("Study Buddies backend running on http://localhost:8080")
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
