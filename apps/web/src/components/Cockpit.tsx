cd3c03a0eea28dd093770428aefe4af4de9fe3e3
Itscoldhere <hougentan@gmail.com>
2026-05-29 14:37:44 +0800

    focusVideo can play video from camera now, change inline CSS to classes for Cockpit.tsx

--- apps/web/src/components/Cockpit.tsx
+++ apps/web/src/components/Cockpit.tsx
@@ -1,15 +1,53 @@
+import { FilesetResolver, ObjectDetector } from "@mediapipe/tasks-vision"
+import { useEffect, useRef, type RefObject } from "react";
+import styles from "./Cockpit.module.css"
+
 function Cockpit() {
+    function LookAtMe() {
+        const objectDetectorRef= useRef<ObjectDetector>(null);
+        const videoRef = useRef<HTMLVideoElement>(null);
+
+        useEffect(() => {
+            const init = async () => {
+                const vision = await FilesetResolver.forVisionTasks(
+                    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.35/wasm"
+                );
+
+                objectDetectorRef.current = await ObjectDetector.createFromOptions(vision, {
+                    baseOptions: {
+                        modelAssetPath: "/models/efficientdet_lite0.tflite"
+                    },
+                    scoreThreshold: 0.5,
+                    runningMode: "VIDEO",
+                    categoryAllowlist: ["cell phone"]
+                })
+            }
+
+            const startCamera = async () => {
+                const stream = await navigator.mediaDevices.getUserMedia({
+                    video: true
+                });
+
+                if (videoRef.current) {
+                    videoRef.current.srcObject = stream;
+                    await videoRef.current.play();
+                }
+            };
+            
+            init();
+            startCamera();
+        }, []);
+
+        return (
+            <div className={styles.focusVideo}>
+                <video ref={videoRef} autoPlay playsInline />
+            </div>
+        )
+    }
+
     function User() {
         return (
-            <div style={{
-                "backgroundColor": "white",
-                "margin": "10px",
-                "height": "70vh",
-                "borderRadius": "50px",
-                "display": "flex",
-                "justifyContent": "center",
-                "alignItems": "center"
-            }}>
+            <div className={styles.focusVideo}>
                 <img src="/social-page.gif" alt="user" style={{"height": "70%"}}/>
             </div>
         )
@@ -17,7 +55,7 @@ function Cockpit() {
 
     return (
         <div>
-            <User />
+            <LookAtMe />
         </div>
     )
 }
